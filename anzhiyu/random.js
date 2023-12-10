247cd450b980c92af8a79020250fe62894bd5737
KongJay <996774254@qq.com>
2023-12-10 23:44:42 +0800

    Site updated: 2023-12-10 23:44:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/10/hello-world/"];function toRandomPost(){
+var posts=["2023/12/10/hello-world/","2023/12/05/后端/vvvvvv/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
